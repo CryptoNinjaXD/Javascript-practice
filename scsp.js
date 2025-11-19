37c86439bf482bf9df2882689018ebc9e47d3e20
CryptoNinja <mayanks444444@gmail.com>
2025-11-19 22:47:27 +0530

    New

---
+++ scsp.js
@@ -0,0 +1,26 @@
+//basic loop
+// for (let i = 0; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// var i = 5;
+// do {
+//   console.log(i);
+//   i--;
+// } while (i > 0);
+
+// let j = 1;
+// for (let i = 5; i > 0; i--) {
+//   j = i * j;
+// }
+// console.log(j);
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+for (i = 0; i < Math.floor(arr.length / 2); i++) {
+  var temp = arr[i];
+  arr[i] = arr[arr.length - 1 - i];
+  arr[arr.length - 1 - i] = temp;
+}
+console.log(arr);
