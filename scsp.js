12ee685f7da7cadfa30a4c6339692ac5a6e1c978
CryptoNinja <mayanks444444@gmail.com>
2025-11-20 14:37:15 +0530

    new

--- scsp.js
+++ scsp.js
@@ -17,10 +17,10 @@
 // }
 // console.log(j);
 
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-for (i = 0; i < Math.floor(arr.length / 2); i++) {
-  var temp = arr[i];
-  arr[i] = arr[arr.length - 1 - i];
-  arr[arr.length - 1 - i] = temp;
-}
-console.log(arr);
+// arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// for (i = 0; i < Math.floor(arr.length / 2); i++) {
+//   var temp = arr[i];
+//   arr[i] = arr[arr.length - 1 - i];
+//   arr[arr.length - 1 - i] = temp;
+// }
+// console.log(arr);
