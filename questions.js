ac272f2f923a9ad23a36ef9e78fcf6bead1178ef
CryptoNinja <mayanks444444@gmail.com>
2025-12-03 19:54:39 +0530

    New

--- questions.js
+++ questions.js
@@ -26,6 +26,33 @@ let step = [];
 const filters = input4.filter((nest) => nest > 10000);
 console.log(JSON.parse(JSON.stringify(filters)));
 console.log(typeof filters);
+
+//q5
+// Expected output: [{name: 'Book', price: 20}, {name: 'Pen', price: 5}, {name: 'Notebook', price: 15}]
+
+const input5 = [
+  { name: "Book", price: 20 },
+  { name: "Pen", price: 5 },
+  { name: "Laptop", price: 800 },
+  { name: "Notebook", price: 15 },
+];
+function check(products, priceMax) {
+  return products.filter((product) => product.price < priceMax);
+}
+const priceUnder50 = check(input5, 50);
+console.log(priceUnder50);
+
+//q6
+//Given an array of objects representing students, calculate the average age of students for each grade
+// Expected output: { '9th': 14.5, '10th': 15.5, '11th': 16 }
+const input6 = [
+  { name: "John", age: 15, grade: "10th" },
+  { name: "Jane", age: 16, grade: "10th" },
+  { name: "Tom", age: 15, grade: "9th" },
+  { name: "Mary", age: 14, grade: "9th" },
+  { name: "Bob", age: 16, grade: "11th" },
+];
+
 //solid squar
 // for (let i = 1; i < 6; i++) {
 //   let row = "";
