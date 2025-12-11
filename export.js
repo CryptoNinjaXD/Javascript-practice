753c83108d84a5de8f4f6c7f5db191cd733d8521
CryptoNinja <mayanks444444@gmail.com>
2025-12-11 14:01:07 +0530

    New

---
+++ export.js
@@ -0,0 +1,30 @@
+// const add = (a, b) => {
+//   return a + b;
+// };
+
+// const subtract = (a, b) => {
+//   return a - b;
+// };
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+
+// const division = (a, b) => {
+//   return a / b;
+// };
+
+// const item = ["Calculator", "Laptop", "Keyboard"];
+
+// module.exports = {
+//   add: add,
+//   subtract: subtract,
+//   multiply: multiply,
+//   division: division,
+//   item: item,
+// };
+
+export const price = 10000;
+
+//class include id, name
+//via class and object
---
+++ module.js
@@ -0,0 +1,9 @@
+// const calculator = require("./export.js");
+// console.log("Addition = ", calculator.add(5, 5));
+// console.log("Subtraction = ", calculator.subtract(5, 5));
+// console.log("Division = ", calculator.division(5, 5));
+// console.log("Multiply = ", calculator.multiply(5, 5));
+// console.log("Items are = ", calculator.item);
+
+import { price } from "./export.js";
+console.log(price);
